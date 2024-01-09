be595cee32249091db4a7ad0f51028712b8ffbc4
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2024-01-09 22:33:25 +0100

    Adding forced static typing

--- addons/gdlogger/logger.gd
+++ addons/gdlogger/logger.gd
@@ -9,37 +9,37 @@ const AUTOLOAD_NAME: String = "LoggerService"
 signal message_received
 
 ## This function logs a generic message with an arbitrary log level.
-func log_message(message: Message):
+func log_message(message: Message) -> void:
 	message_received.emit(message)
 
 ## Debug logs at the debug log level. This should be used sparingly only if needed.
-func debug(_message: String):
+func debug(_message: String) -> void:
 	log_message(Message.new(Logger.Level.DEBUG, Time.get_unix_time_from_system(), _message))
 	print_verbose("%s\tDEBUG\t%s"%[Time.get_datetime_string_from_system(), _message])
 
 ## This function logs informational, but still important messages.
-func info(_message: String):
+func info(_message: String) -> void:
 	log_message(Message.new(Logger.Level.INFO, Time.get_unix_time_from_system(), _message))
 	print("%s\tINFO\t%s"%[Time.get_datetime_string_from_system(), _message])
 
 ## This function logs warning messages that convey a potentially problematic information.
-func warning(_message: String):
+func warning(_message: String) -> void:
 	log_message(Message.new(Logger.Level.WARNING, Time.get_unix_time_from_system(), _message))
 	printerr("%s\tWARNING\t%s"%[Time.get_datetime_string_from_system(),_message])
 
 ## This function logs an error condition that a log inspection should treat with special care. This function will
 ## not stop the program execution.
-func error(_message: String):
+func error(_message: String) -> void:
 	log_message(Message.new(Logger.Level.ERROR, Time.get_unix_time_from_system(), _message))
 	printerr("%s\tERROR\t%s"%[Time.get_datetime_string_from_system(), _message])
 
 ## Helper function to retrieve the autoloaded Logger service. If the requester is not in a tree this function
 ## will return null.
 static func get_service(requester: Node) -> Logger:
-	var tree = requester.get_tree()
+	var tree: SceneTree = requester.get_tree()
 	if tree == null:
 		return null
-	var root = tree.get_root()
+	var root: Node = tree.get_root()
 	if root == null:
 		return null
 	return root.get_node(AUTOLOAD_NAME)
@@ -58,7 +58,7 @@ class Message:
 	var time: float
 	var message: String
 
-	func _init(_level: Logger.Level, _time: float, _message: String):
+	func _init(_level: Logger.Level, _time: float, _message: String) -> void:
 		level = _level
 		time = _time
 		message = _message
